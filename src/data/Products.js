92e7489564c40497e056cc5ec2eeb92142034d03
Candeb <candebustos084@gmail.com>
2023-08-23 23:12:07 -0300

    cambio descr a description

--- src/data/Products.js
+++ src/data/Products.js
@@ -4,7 +4,7 @@ export const products = [
     name: 'Nike Air Max x Travis Scott ',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/nike_ft_travis_scott.png?raw=true',
-    descr: 'Air Max 1 low-top sneakers',
+    description: 'Air Max 1 low-top sneakers',
     price: 540,
     category: 'Nike',
   },
@@ -13,7 +13,7 @@ export const products = [
     name: 'Nike Air Foamposite',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/nike%20air%20fomposite%20.png?raw=true',
-    descr: 'Air Foamposite One NRG sneakers',
+    description: 'Air Foamposite One NRG sneakers',
     price: 3240,
     category: 'Nike',
   },
@@ -22,7 +22,7 @@ export const products = [
     name: 'Nike Air Max x Sean Wotherspoon',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/nike%20x%20Sean%20Wotherspoon.png?raw=true',
-    descr: 'Air Max 1/97 VF sneakers',
+    description: 'Air Max 1/97 VF sneakers',
     price: 1850,
     category: 'Nike',
   },
@@ -31,7 +31,8 @@ export const products = [
     name: 'Nike Air Force 1 Low x Swarovski ',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Nike%20x%20Swarovski%20.png?raw=true',
-    descr: "Air Force 1 Low LXX 'Retroreflective Crystals' White sneakers",
+    description:
+      "Air Force 1 Low LXX 'Retroreflective Crystals' White sneakers",
     price: 1920,
     category: 'Nike',
   },
@@ -40,7 +41,7 @@ export const products = [
     name: 'Nike Air Uptempo',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/nike%20Air%20More%20Uptempo.png?raw=true',
-    descr: 'Air More Uptempo sneakers',
+    description: 'Air More Uptempo sneakers',
     price: 1180,
     category: 'Nike',
   },
@@ -49,7 +50,7 @@ export const products = [
     name: 'Nike Air Fear Of God',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/nike%20altas%20Air%20Fear%20Of%20God%201.png?raw=true',
-    descr: 'Air Fear Of God 1 hi-top sneakers',
+    description: 'Air Fear Of God 1 hi-top sneakers',
     price: 1560,
     category: 'Nike',
   },
@@ -58,7 +59,7 @@ export const products = [
     name: 'Adidas NMD R1 Bape',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/adidas%20NMD_R1%20Bape.png?raw=true',
-    descr: 'Adidas NMD R1 Bape Olive Camo sneakers',
+    description: 'Adidas NMD R1 Bape Olive Camo sneakers',
     price: 1385,
     category: 'Adidas',
   },
@@ -67,7 +68,7 @@ export const products = [
     name: 'Air Jordan 4 Retro Kaws',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Jordan%204%20Retro%20Kaws.png?raw=true',
-    descr: 'Nike Air Jordan 4 Retro Kaws grey',
+    description: 'Nike Air Jordan 4 Retro Kaws grey',
     price: 5920,
     category: 'Nike',
   },
@@ -76,7 +77,7 @@ export const products = [
     name: 'Air Jordan x Travis Scott',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Jordan%201%20Hi%20OG%20de%20Jordan%20x%20Travis%20Scott.png?raw=true',
-    descr: 'Nike Air Jordan 1 x Travis Scott Hi OG sneakers',
+    description: 'Nike Air Jordan 1 x Travis Scott Hi OG sneakers',
     price: 3330,
     category: 'Nike',
   },
@@ -85,7 +86,7 @@ export const products = [
     name: 'Air Jordan 5 Retro x Supreme',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Jordan%205%20Retro%20Supreme.png?raw=true',
-    descr: 'Nike Air Jordan 5 Retro Supreme Camo sneakers',
+    description: 'Nike Air Jordan 5 Retro Supreme Camo sneakers',
     price: 1480,
     category: 'Nike',
   },
@@ -94,7 +95,7 @@ export const products = [
     name: 'Balenciaga X-Pander',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20X-Pander%20con%20cordones.png?raw=true',
-    descr: 'Balenciaga X-Pander lace-up',
+    description: 'Balenciaga X-Pander lace-up',
     price: 1215,
     category: 'Balenciaga',
   },
@@ -103,7 +104,7 @@ export const products = [
     name: 'Balenciaga Defender',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20bajas%20defender.png?raw=true',
-    descr: 'Balenciaga Defender low-top',
+    description: 'Balenciaga Defender low-top',
     price: 1195,
     category: 'Balenciaga',
   },
@@ -112,7 +113,7 @@ export const products = [
     name: 'Balenciaga Triple S',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20bajas%20Triple%20S.png?raw=true',
-    descr: 'Balenciaga Triple S low-top',
+    description: 'Balenciaga Triple S low-top',
     price: 1195,
     category: 'Balenciaga',
   },
@@ -121,7 +122,7 @@ export const products = [
     name: 'Balenciaga Triple S logo printed',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20Triple%20S%20con%20logo%20estampado.png?raw=true',
-    descr: 'Balenciaga White Triple S logo print low-top sneakers',
+    description: 'Balenciaga White Triple S logo print low-top sneakers',
     price: 1190,
     category: 'Balenciaga',
   },
@@ -130,7 +131,7 @@ export const products = [
     name: 'Balenciaga Speed',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20Speed%20Clear%20Sole.png?raw=true',
-    descr: 'Balenciaga Speed Clear Sole sneakers',
+    description: 'Balenciaga Speed Clear Sole sneakers',
     price: 1150,
     category: 'Balenciaga',
   },
@@ -139,7 +140,7 @@ export const products = [
     name: 'Balenciaga Triple S faded',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20Triple%20S%20con%20efecto%20desgastado.png?raw=true',
-    descr: "Balenciaga Triple S 'Faded Black' sneakers",
+    description: "Balenciaga Triple S 'Faded Black' sneakers",
     price: 1190,
     category: 'Balenciaga',
   },
@@ -148,7 +149,7 @@ export const products = [
     name: 'Balenciaga Triple S',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/balenciaga/balenciaga%20Triple%20S.png?raw=true',
-    descr: 'Balenciaga Triple S sneakers',
+    description: 'Balenciaga Triple S sneakers',
     price: 1325,
     category: 'Balenciaga',
   },
@@ -157,7 +158,7 @@ export const products = [
     name: 'Adidas x Pharrell Williams',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/adidas%20x%20Pharrell%20Williams.png?raw=true',
-    descr:
+    description:
       "Adidas x Billionaire Boys Club x Pharrell Hu NMD 'Astronaut Blue' sneakers",
     price: 260,
     category: 'Adidas',
@@ -167,7 +168,7 @@ export const products = [
     name: 'Nike Dunk x Cactus Plant x Swarovski',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Dunk%20de%20Nike%20x%20Cactus%20Plant%20Flea%20Market%20x%20Swarovski.png?raw=true',
-    descr: 'Dunk de Nike x Cactus Plant Flea Market x Swarovski',
+    description: 'Dunk de Nike x Cactus Plant Flea Market x Swarovski',
     price: 13870,
     category: 'Nike',
   },
@@ -176,7 +177,7 @@ export const products = [
     name: 'Air Jordan x Dior',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Jordan%201%20de%20Jordan%20x%20Dior.png?raw=true',
-    descr: 'Nike Air Jordan 1 x Dior Low sneakers',
+    description: 'Nike Air Jordan 1 x Dior Low sneakers',
     price: 19140,
     category: 'Nike',
   },
@@ -185,7 +186,7 @@ export const products = [
     name: 'Speed Balenciaga x Adidas',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Speed%20de%20Balenciaga%20x%20adidas.png?raw=true',
-    descr: 'Balenciaga x adidas Speed high-top sneakers',
+    description: 'Balenciaga x adidas Speed high-top sneakers',
     price: 1200,
     category: 'Balenciaga',
   },
@@ -194,7 +195,7 @@ export const products = [
     name: 'Nike Air Force 1 x Louis Vuitton',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Nike%20x%20Louis%20Vuitton.png?raw=true',
-    descr:
+    description:
       "Nike Air Force 1 x Louis Vuitton 'Virgil Abloh - White/White' sneakers",
     price: 9615,
     category: 'Nike',
@@ -204,7 +205,7 @@ export const products = [
     name: 'Nike x Supreme',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Shox%20Ride%202%20SP%20de%20Nike%20x%20Supreme.png?raw=true',
-    descr: 'Nike x Supreme Shox Ride 2 SP sneakers',
+    description: 'Nike x Supreme Shox Ride 2 SP sneakers',
     price: 1305,
     category: 'Nike',
   },
@@ -213,7 +214,7 @@ export const products = [
     name: 'Adidas Forum Buckle x Bad Bunny ',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Forum%20Buckle%20Low%20de%20adidas%20x%20Bad%20Bunny.png?raw=true',
-    descr: "Adidas x Bad Bunny 'Forum Buckle White' sneakers",
+    description: "Adidas x Bad Bunny 'Forum Buckle White' sneakers",
     price: 345,
     category: 'Adidas',
   },
@@ -222,7 +223,7 @@ export const products = [
     name: 'Nike x PlayStation',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/PG%205%20PlayStation%20Blue%20de%20Nike%20x%20PlayStation.png?raw=true',
-    descr: "Nike x PlayStation PG 5 sneakers 'PlayStation Blue' sneakers",
+    description: "Nike x PlayStation PG 5 sneakers 'PlayStation Blue' sneakers",
     price: 330,
     category: 'Nike',
   },
@@ -231,7 +232,8 @@ export const products = [
     name: 'Nike x Comme Des Garçons',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Swoosh%20de%20Nike%20x%20Comme%20Des%20Gar%C3%A7ons.png?raw=true',
-    descr: 'Nike x Comme Des Garçons Homme Plus swoosh-logo detail sneakers',
+    description:
+      'Nike x Comme Des Garçons Homme Plus swoosh-logo detail sneakers',
     price: 640,
     category: 'Nike',
   },
@@ -240,7 +242,7 @@ export const products = [
     name: 'Nike x Concepts',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Max%201%20Mellow%20de%20Nike%20x%20Concepts.png?raw=true',
-    descr: "Nike x Concepts Air Max 1 'Mellow' sneakers",
+    description: "Nike x Concepts Air Max 1 'Mellow' sneakers",
     price: 330,
     category: 'Nike',
   },
@@ -249,7 +251,7 @@ export const products = [
     name: 'Nike Dunk x Quartersnacks',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Dunk%20Low%20SB%20OG%20de%20Nike%20x%20Quartersnacks.png?raw=true',
-    descr: "Nike SB Dunk Low OG QS 'Quarter Snacks' sneakers",
+    description: "Nike SB Dunk Low OG QS 'Quarter Snacks' sneakers",
     price: 400,
     category: 'Nike',
   },
@@ -258,7 +260,7 @@ export const products = [
     name: 'Nike x Drake',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Hot%20Step%20Air%20Terra%20Nocta%20de%20Nike%20x%20Drake.png?raw=true',
-    descr: "Nike x Drake Hot Step Air Terra NOCTA 'White' sneakers",
+    description: "Nike x Drake Hot Step Air Terra NOCTA 'White' sneakers",
     price: 380,
     category: 'Nike',
   },
@@ -267,7 +269,7 @@ export const products = [
     name: 'Nike X Off-White',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Terra%20Forma%20de%20Nike%20x%20Off-White.png?raw=true',
-    descr: "Nike Air x Off-White Terra Forma 'Wheat' sneakers",
+    description: "Nike Air x Off-White Terra Forma 'Wheat' sneakers",
     price: 685,
     category: 'Nike',
   },
@@ -276,7 +278,7 @@ export const products = [
     name: 'Nike x Supreme Flight 95',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Zoom%20Flight%2095%20de%20Nike%20x%20Supreme.png?raw=true',
-    descr: 'Nike Air x Supreme Air Zoom Flight 95 sneakers',
+    description: 'Nike Air x Supreme Air Zoom Flight 95 sneakers',
     price: 420,
     category: 'Nike',
   },
@@ -285,7 +287,7 @@ export const products = [
     name: 'Nike x Ambush',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Nike%20x%20Ambush.png?raw=true',
-    descr: 'Nike Air x Ambush Adjust Force sneakers',
+    description: 'Nike Air x Ambush Adjust Force sneakers',
     price: 420,
     category: 'Nike',
   },
@@ -294,7 +296,7 @@ export const products = [
     name: 'Adidas x Bad Bunny Benito',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Forum%20Powerhouse%20Benito%20de%20adidas%20x%20Bad%20Bunny.png?raw=true',
-    descr: "Adidas x Bad Bunny Forum Powerhouse sneakers 'Benito'",
+    description: "Adidas x Bad Bunny Forum Powerhouse sneakers 'Benito'",
     price: 355,
     category: 'Adidas',
   },
@@ -303,7 +305,7 @@ export const products = [
     name: 'Adidas x Bad Bunny Easter Egg',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Forum%20Low%20Easter%20Egg%20de%20adidas%20x%20Bad%20Bunny.png?raw=true',
-    descr: "Adidas x Bad Bunny Forum Low 'Easter Egg' sneakers",
+    description: "Adidas x Bad Bunny Forum Low 'Easter Egg' sneakers",
     price: 1095,
     category: 'Adidas',
   },
@@ -312,7 +314,7 @@ export const products = [
     name: 'Adidas x Parley Shoes',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Forum%20Mid%20de%20adidas%20x%20Parley%20Shoes.png?raw=true',
-    descr: 'Adidas x Parley Shoes Forum Mid sneakers',
+    description: 'Adidas x Parley Shoes Forum Mid sneakers',
     price: 155,
     category: 'Adidas',
   },
@@ -321,7 +323,7 @@ export const products = [
     name: 'Balenciaga Triple S x Adidas',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/Triple%20S%20de%20Balenciaga%20x%20adidas.png?raw=true',
-    descr: 'Triple S Black Balenciaga x Adidas sneakers',
+    description: 'Triple S Black Balenciaga x Adidas sneakers',
     price: 1380,
     category: 'Balenciaga',
   },
@@ -330,7 +332,7 @@ export const products = [
     name: 'Adidas AdiFOM Q',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/adidas%20adiFOM%20Q.png?raw=true',
-    descr: "Adidas AdiFOM Q 'Footwear White Grey' sneakers",
+    description: "Adidas AdiFOM Q 'Footwear White Grey' sneakers",
     price: 245,
     category: 'Adidas',
   },
@@ -339,7 +341,7 @@ export const products = [
     name: 'Adidas Attitude Hi',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/adidas%20attitude%20hi.png?raw=true',
-    descr: 'Adidas Big Sean Attitude Hawaii Metro Pro sneakers',
+    description: 'Adidas Big Sean Attitude Hawaii Metro Pro sneakers',
     price: 880,
     category: 'Adidas',
   },
@@ -348,7 +350,7 @@ export const products = [
     name: 'Adidas x Raf Simons',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/adidas%20Ozweego.png?raw=true',
-    descr: 'Adidas x Raf Simons Ozweego sneakers',
+    description: 'Adidas x Raf Simons Ozweego sneakers',
     price: 920,
     category: 'Adidas',
   },
@@ -357,7 +359,7 @@ export const products = [
     name: 'Nike Air Force 1 Mid x Louis Vuitton',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/nike/Air%20Force%201%20Mid%20de%20Nike%20x%20Louis%20Vuitton.png?raw=true',
-    descr: 'Nike Air Force 1 Mid x Louis Vuitton Graffiti sneakers',
+    description: 'Nike Air Force 1 Mid x Louis Vuitton Graffiti sneakers',
     price: 36970,
     category: 'Nike',
   },
@@ -366,7 +368,8 @@ export const products = [
     name: 'Adidas X Speedportal Leyenda',
     picture:
       'https://github.com/Candeb/jsx-store-frontend/blob/main/src/assets/sneakers/adidas/botines%20X%20Speedportal%20Leyenda%20de%20Messi.png?raw=true',
-    descr: 'Bota de fútbol Leyenda Lionel Messi 2022 World Cup Boots HP9138',
+    description:
+      'Bota de fútbol Leyenda Lionel Messi 2022 World Cup Boots HP9138',
     price: 181222,
     category: 'Adidas',
   },
